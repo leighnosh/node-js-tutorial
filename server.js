9fee37f9b78502562f015a4dc0abf062c3660836
leighnosh <leighnosh13@gmail.com>
2023-11-01 11:05:53 +0530

    Redirecting Requests and Parsing Request Bodies

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((request, response) => {
-  const url = request.url;
-
-  if (url === "/home") {
-    response.setHeader("Content-Type", "text/html");
-    response.write("<h1>Welcome home</h1>");
-    response.end();
-  } else if (url === "/about") {
-    response.setHeader("Content-Type", "text/html");
-    response.write("<h1>Welcome to About Us page</h1>");
-    response.end();
-  } else if (url === "/node") {
-    response.setHeader("Content-Type", "text/html");
-    response.write("<h1>Welcome to my Node Js project</h1>");
-    response.end();
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt", "utf8", (err, data) => {
+      res.write(`<h1>${data}</h1>`);
+      res.write(
+        '<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
+      );
+      res.write("</body></html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
   } else {
-    response.setHeader("Content-Type", "text/html");
-    response.write("<h1>Page not found</h1>");
-    response.end();
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
+    res.end();
   }
 });
 
