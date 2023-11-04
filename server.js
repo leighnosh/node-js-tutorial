8c35bcc5944e0db70f208624bb571ee8bdee9b47
leighnosh <leighnosh13@gmail.com>
2023-11-04 13:25:15 +0530

    Riuting and parsing usig express

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('Inside the middleware!');
-    next();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="product", method = "POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>'
+  );
 });
-app.use((req, res, next) => {
-    console.log('Inside another middleware!');
-    res.send('Hello from Express');
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
 });
 
 app.listen(3000);
